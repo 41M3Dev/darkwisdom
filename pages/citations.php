147ffdd157c854111b3a62115d1cc5d79f02a500
41M3Dev <102484378+41M3Dev@users.noreply.github.com>
2025-02-13 15:17:12 +0100

    FIX : responsive du header

--- pages/citations.php
+++ pages/citations.php
@@ -40,12 +40,26 @@
           <ul class="navbar-nav ms-auto">
             <li class="nav-item"><a class="nav-link" href="../index.php">Accueil</a></li>
             <li class="nav-item"><a class="nav-link" href="citations.php">Citations</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">Inscription</a></li>
-            <li class="nav-item"><a class="nav-link sp" href="#">Connexion</a></li>
-            <!-- <li class="nav-item"><a class="nav-link" href="#">Favoris</a></li> -->
-            <!-- <li class="nav-item ms-3">
-              <a class="nav-link" href="#"><i class="fa-regular fa-user"></i></a>
-            </li> -->
+             <?php
+             // Démarrer la session si ce n'est pas déjà fait
+             if (session_status() === PHP_SESSION_NONE) {
+                 session_start();
+             }
+             // Vérifier si l'utilisateur est connecté
+              if (isset($_SESSION['user_id'])) {
+              // Si l'utilisateur est connecté
+              echo '<li class="nav-item"><a class="nav-link" href="profil.php">Favoris</a></li>
+              <li class="nav-item"><a class="nav-link" href="logout.php">Déconnexion</a></li>
+              <li class="nav-item ms-3">
+                  <a class="nav-link" href="profil.php"><i class="fa-regular fa-user"></i></a>
+              </li>';
+              } else {
+              // Si l'utilisateur n'est pas connecté
+              echo '
+              <li class="nav-item"><a class="nav-link" href="register.php">Inscription</a></li>
+              <li class="nav-item"><a class="nav-link sp" href="login.php">Connexion</a></li>';
+              }
+              ?>
           </ul>
         </div>
       </div>
